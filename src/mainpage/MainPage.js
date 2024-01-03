e61aca60742641e4b9e017758383543dc386c11c
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-01-03 18:04:04 +0100

    MainPage css done.

--- src/mainpage/MainPage.js
+++ src/mainpage/MainPage.js
@@ -1,10 +1,30 @@
+import { Divider } from 'primereact/divider';
+import { Card } from 'primereact/card';
+import './MainPage.css'
 
 export function MainPage() {
-
+      
 
     return (
         <div id="homePage">
+            <Card>
+                <div id="menu">
+                    <div id="menuList">
+                        <h1>LanGo</h1>
+                        <p>Home</p>
+                        <p>Search</p>
+                        <p>Profile</p>
+                        <Divider/>
+                        <p>Log out</p>
+                    </div>
 
+                    <div id="currentUser">
+                        <img src={process.env.PUBLIC_URL + '/assets/person.png'} alt="img"/>
+                        <h2>Max</h2>
+                    </div>
+                </div>
+                
+            </Card>
         </div>
     )
 }
