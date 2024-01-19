2c5a41ec76fa3a97f386be79b86e94872225ddad
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-01-19 13:05:33 +0100

    Animated the flags.

--- src/view/login/LoginPage.js
+++ src/view/login/LoginPage.js
@@ -10,16 +10,18 @@ export function LoginPage() {
 
   const countries = ["croatia", "france", "germany", "italy", "japan", "usa", "india", "croatia", "great-britain", "russian-federation", "china"];
 
-    return (
-        <div id="loginPage">
-          <animated.div id="loginTexts" style={{ ...springs }}>
-            <AppTitle/>
-            <LoginForm/>
-          </animated.div>
+  return (
+    <div id="loginPage">
+      <animated.div id="loginTexts" style={{ ...springs }}>
+        <AppTitle />
+        <LoginForm />
+      </animated.div>
 
-          <animated.div id="flags" style={{ ...springs2 }}>
-            {countries.map((country) => <img src={process.env.PUBLIC_URL + `/assets/flags/icons8-${country}-96.png`} alt="img"/>)}
-          </animated.div>
-        </div>
-    )
+      <animated.div id="flags" style={{ ...springs2 }}>
+        {countries.map((country, index) => ( 
+          <img src={process.env.PUBLIC_URL + `/assets/flags/icons8-${country}-96.png`} alt="img" style={{ animationDelay: `${index*0.1 + 1}s` }}/>
+        ))}
+      </animated.div>
+    </div>
+  );
 }
